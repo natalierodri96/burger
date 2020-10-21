1ff344ae1550db1bc3cdf98eb4549417fda850e3
Natalie Rodriguez <natalierodri96@gmail.com>
2020-10-21 14:52:48 -0500

    orms

---
+++ config/orms.js
@@ -0,0 +1,37 @@
+const connection = require("./connection")
+const { connect } = require("./connection")
+
+var orm = {
+    updateAll: function() {
+        connection.query("UPDATE FROM burgers WHERE id = ?", [id], function(err,res){
+            //code?
+            if (err) {
+                throw err;
+            }
+            callback(res);
+        });
+    };
+    
+    insertOne: function() {
+        connection.query("INSERT ONE FROM burgers WHERE id = ?", [id], function(err,res){
+            //code?
+            if (err) {
+                throw err;
+                callback(res);
+            }
+        });
+    };
+
+    selectOne: function() {
+        connection.query("SELECT ONE FROM burgers WHERE id = ?", [id], function(err,res){
+            //code?
+            if (err) {
+                throw err;
+                callback(res); 
+            }
+        });
+    };
+}
+
+
+module.exports = orm;

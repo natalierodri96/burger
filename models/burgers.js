cc19efd008bda1add308ed2ee1d976a2e681f4ec
Natalie Rodriguez <natalierodri96@gmail.com>
2020-10-21 18:13:20 -0500

    burgersjs

---
+++ models/burgers.js
@@ -0,0 +1,20 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    create: function(burger_name, cb) {
+        orm.insertOne("burgers", ["burger_name"], [burgerName],function(res) {
+            cb(res);   
+        });
+    },
+    update: function(objoVals, conditon, cb) {
+        orm.updateOne("burgers", objoVals, conditon, function (res) {
+            cb(res);
+        });
+    }
+};
+module.exports = burger;

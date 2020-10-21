92ae7176616fb4b457c81073724a24f8dc1a9a1a
Natalie Rodriguez <natalierodri96@gmail.com>
2020-10-21 14:30:38 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("They told", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("They said", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("They say", FALSE);

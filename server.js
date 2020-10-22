d66854ee158b4d015d25d083f14d4d694a178fc8
Natalie Rodriguez <natalierodri96@gmail.com>
2020-10-21 21:57:45 -0500

    serverjs

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
 
 var express = require("express");
-
-// Sets up the Express App
-// =============================================================
+var bodyParser =("body-parser");
+var exphbs = require("express-handlebars");
 var app = express();
-var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Static directory to be served
-app.use(express.static("app/public"));
-
-// Routes
-// =============================================================
-require("./app/routes/api-routes.js")(app);
-
-// Starts the server to begin listening
-// =============================================================
+var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+    console.log("server listening on: https://localhost:" + PORT)
+})
+
